e04ae535cbe614b7b1c160ef034818aa31331a50
João Gabriel Royo <joao.royosilva@gmail.com>
2014-10-17 10:18:57 -0400

    Ver1

---
+++ site/index.php
@@ -0,0 +1,242 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Medical Center</title>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+        <!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+        <script src="js/jquery.min.js"></script>
+        <script src="js/jquery.dropotron.min.js"></script>
+        <script src="js/jquery.scrolly.min.js"></script>
+        <script src="js/jquery.onvisible.min.js"></script>
+        <script src="js/skel.min.js"></script>
+        <script src="js/skel-layers.min.js"></script>
+        <script src="js/init.js"></script>
+        <noscript>
+        <link rel="stylesheet" href="css/skel.css" />
+        <link rel="stylesheet" href="css/style.css" />
+        <link rel="stylesheet" href="css/style-desktop.css" />
+        <link rel="stylesheet" href="css/style-noscript.css" />
+        </noscript>
+        <!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+    </head>
+    <body class="homepage">
+
+        <!-- Header -->
+        <div id="header">
+            <!-- Inner -->
+            <div class="inner">
+                <header>
+                    <h1><a href="index.html" id="logo">Medical Center</a></h1>
+                    <hr />
+                    <p>Clínica de especialidades médicas & diagnósticos por imagens</p>
+                </header>
+                <footer>
+                    <a href="#banner" class="button scrolly">Veja mais!</a>
+                </footer>
+            </div>
+            <!-- Nav -->
+            <nav id="nav">
+                <ul>
+                    <li><a href="#banner" class="scrolly">Sobre nós</a></li>
+                    <li><a href="#professionals" class="scrolly">Profissionais</a></li>
+                    <li><a href="#features" class="scrolly">Especialidades</a></li>
+                    <li><a href="#footer" class="scrolly">Contato</a></li>
+                </ul>
+            </nav>
+
+        </div>
+
+        <!-- Banner -->
+        <section id="banner">
+            <header>
+                <h2>Olá. Conheça um pouco sobre a <strong>Medical Center</strong>.</h2>
+                <p>
+                    A <strong>Medical Center</strong> é fruto de um empreendimento realizado por oito parceiros de diversas áreas.
+                    Foi inaugurada na cidade de Três Lagoas para se tornar um centro de referência de <strong>atendimento</strong> e 
+                    <strong>qualidade</strong> na área de saúde para seus pacientes através de maior <strong>atenção</strong>, 
+                    <strong>dedicação</strong> e <strong>cuidados</strong> pelos diversos 
+                    especialistas que compõem seu corpo clínico.
+                </p>
+                <p>
+                    O projeto para esse grande centro médico teve início no ano de 2010 e se concretizou no 
+                    dia 27 de setembro do ano de 2013, sendo sua inauguração.
+                </p>    
+                <p>
+                    Na Medical Center, o paciente encontrará <strong>especialistas</strong> de urologia, cirurgia gástrica, 
+                    gastroendoscopia, colonoscopia, cardiologia, ortopedia, cirurgia geral, dermatologia,
+                    endocrinologia, pediatria, ginecologia, mastologia, clínica médica, odontologia, nutrição, 
+                    radiologia, diagnósticos por imagem, raio X digital, densitometria óssea, ultrassonografia, 
+                    eco cardiograma, dopler, mapa, holter, eletrocardiograma e acoplado à sua estrutura. Há, também, um 
+                    laboratório de análises clínicas para contribuir com uma maior <strong>agilidade</strong> nos resultados dos exames.
+                </p>
+            </header>
+        </section>
+
+        <!-- Carousel -->
+        <section class="carousel" id="professionals">
+            <div class="reel">
+
+                <article>
+                    <a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
+                    <header>
+                        <h3><a href="#">Dr. Rafael Tibyriçá</a></h3>
+                    </header>
+                    <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>							
+                </article>
+
+                <article>
+                    <a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
+                    <header>
+                        <h3><a href="#">Fermentum sagittis proin</a></h3>
+                    </header>
+                    <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>							
+                </article>
+
+                <article>
+                    <a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
+                    <header>
+                        <h3><a href="#">Sed quis rhoncus placerat</a></h3>
+                    </header>
+                    <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>							
+                </article>
+
+                <article>
+                    <a href="#" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
+                    <header>
+                        <h3><a href="#">Ultrices urna sit lobortis</a></h3>
+                    </header>
+                    <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>							
+                </article>
+
+                <article>
+                    <a href="#" class="image featured"><img src="images/pic05.jpg" alt="" /></a>
+                    <header>
+                        <h3><a href="#">Varius magnis sollicitudin</a></h3>
+                    </header>
+                    <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>							
+                </article>
+
+                <article>
+                    <a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
+                    <header>
+                        <h3><a href="#">Pulvinar sagittis congue</a></h3>
+                    </header>
+                    <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>							
+                </article>
+
+                <article>
+                    <a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
+                    <header>
+                        <h3><a href="#">Fermentum sagittis proin</a></h3>
+                    </header>
+                    <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>							
+                </article>
+
+                <article>
+                    <a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
+                    <header>
+                        <h3><a href="#">Sed quis rhoncus placerat</a></h3>
+                    </header>
+                    <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>							
+                </article>
+
+                <article>
+                    <a href="#" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
+                    <header>
+                        <h3><a href="#">Ultrices urna sit lobortis</a></h3>
+                    </header>
+                    <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>							
+                </article>
+
+                <article>
+                    <a href="#" class="image featured"><img src="images/pic05.jpg" alt="" /></a>
+                    <header>
+                        <h3><a href="#">Varius magnis sollicitudin</a></h3>
+                    </header>
+                    <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>							
+                </article>
+
+            </div>
+        </section>
+
+        <!-- Features -->
+        <div class="wrapper style1">
+
+            <section id="features" class="container special">
+                <header>
+                    <h2>Morbi ullamcorper et varius leo lacus</h2>
+                    <p>Ipsum volutpat consectetur orci metus consequat imperdiet duis integer semper magna.</p>
+                </header>
+                <div class="row">
+                    <article class="4u special">
+                        <a href="#" class="image featured"><img src="images/pic07.jpg" alt="" /></a>
+                        <header>
+                            <h3><a href="#">Gravida aliquam penatibus</a></h3>
+                        </header>
+                        <p>
+                            Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
+                            porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
+                        </p>
+                    </article>
+                    <article class="4u special">
+                        <a href="#" class="image featured"><img src="images/pic08.jpg" alt="" /></a>
+                        <header>
+                            <h3><a href="#">Sed quis rhoncus placerat</a></h3>
+                        </header>
+                        <p>
+                            Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
+                            porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
+                        </p>
+                    </article>
+                    <article class="4u special">
+                        <a href="#" class="image featured"><img src="images/pic09.jpg" alt="" /></a>
+                        <header>
+                            <h3><a href="#">Magna laoreet et aliquam</a></h3>
+                        </header>
+                        <p>
+                            Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
+                            porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
+                        </p>
+                    </article>
+                </div>
+            </section>
+
+        </div>
+
+        <!-- Footer -->
+        <div id="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="12u">
+
+                        <!-- Contact -->
+                        <section class="contact">
+                            <header>
+                                <h3>Faça-nos uma visita!</h3>
+                            </header>
+                            <p>Endereço: Rua João Dantas Filgueiras nº 1185, São Carlos - Três Lagoas - MS<br>
+                                Telefone: (67)3524-4004<br>
+                                Email: contato@medicalcenterms.com.br</p>
+                            <ul class="icons">
+                                <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+                                <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+                            </ul>
+                        </section>
+
+                        <!-- Copyright -->
+                        <div class="copyright">
+                            <ul class="menu">
+                                <li>Desenvolvido por: <a href="http://www.suportec3l.com.br" target="blank">Suportec Informática</a></li>
+                            </ul>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </body>
+</html>
